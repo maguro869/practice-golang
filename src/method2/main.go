9ee6808019cf736be582fd4708cd1f3a7870ee0d
maguro869 <maguro869negitoro@gmail.com>
2021-02-04 12:05:00 +0900

    [UPDATE]methodの範囲に留めた

--- src/method2/main.go
+++ src/method2/main.go
@@ -14,33 +14,24 @@ type Human struct {
 type Student struct {
 	Human //匿名フィールド
 	school string
-	loan float32
 }
 // Employee struct
 type Employee struct {
 	Human //匿名フィールド
 	company string
-	money float32
 }
 
 // SayHi method
 func (h * Human) SayHi() {
 	fmt.Printf("Hi, I am %s you can call me on %s\n", h.name, h.phone)
 }
-// Sing method
-func (h * Human) Sing(lyrics string) {
-	fmt.Println("La la, la la la, la la la la la...", lyrics)
-}
-
-// Guzzle method
-func (h *Human) Guzzle(beerStein string) {
-	fmt.Println("Guzzle Guzzle Guzzle...", beerStein)
-}
 
 // SayHi Employee edition
 func (e *Employee) SayHi() {
 	fmt.Printf("Hi, I am %s, I work at %s. Call me on %s\n", e.name, e.company, e.phone)
 }
+
+
 
 func main() {
 	mark := Student{Human{"Mark", 25, "222-222-YYYY"}, "MIT"}
