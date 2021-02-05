f589fbe4d3867a0909a6a02ea2f9b78e0c7d6e4b
maguro869 <maguro869negitoro@gmail.com>
2021-02-05 22:06:31 +0900

    [ADD]空のinterfaceに入れたのが何型かが判断できるよ

---
+++ src/interface2/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// Element 空のinterface
+type Element interface{}
+
+// List Element型
+type List [] Element
+
+// Person struct
+type Person struct {
+	name string
+	age int
+}
+
+// Stringメソッドを定義 fmt.Stringerを実装
+func (p Person) String() string {
+	return "(name: "+ p.name + " - age: "+strconv.Itoa(p.age) + " years)"
+}
+
+func main() {
+	list := make(List, 3)
+	list[0] = 1 	// これはint
+	list[1] = "Hello"	// これはstring
+	list[2] = Person{"Dennis", 70}	// これはPerson
+
+	for index, element := range list {
+		if value, ok := element.(int); ok {
+			fmt.Printf("list[%d] is an int and its value is %d\n", index, value)
+		} else if value, ok := element.(string); ok {
+			fmt.Printf("list[%d] is a string and its value is %s\n", index, value)
+		} else if value, ok := element.(Person); ok {
+			fmt.Printf("list[%d] is a Person and its value is %s\n", index, value)
+		} else {
+			fmt.Printf("list[%d] is of a different type\n", index)
+		}
+	}
+}
