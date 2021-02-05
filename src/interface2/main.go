91567a7b96afdfd36f9a6bf43288f5cfa80a2b08
maguro869 <maguro869negitoro@gmail.com>
2021-02-05 22:14:01 +0900

    [UPDATE]switchテストも試した

--- src/interface2/main.go
+++ src/interface2/main.go
@@ -29,6 +29,7 @@ func main() {
 	list[2] = Person{"Dennis", 70}	// これはPerson
 
 	// "Comma-okアサーション"で、型を判断する
+	fmt.Println("------------Comma-okアサーション------------")
 	for index, element := range list {
 		if value, ok := element.(int); ok {
 			fmt.Printf("list[%d] is an int and its value is %d\n", index, value)
@@ -38,6 +39,23 @@ func main() {
 			fmt.Printf("list[%d] is a Person and its value is %s\n", index, value)
 		} else {
 			fmt.Printf("list[%d] is of a different type\n", index)
+		}
+	}
+
+	fmt.Println()
+
+	// "switchテスト"で、型を判断する
+	fmt.Println("----------------switchテスト----------------")
+	for index, element := range list {
+		switch value := element.(type) {
+			case int:
+				fmt.Printf("list[%d] is an int and its value is %d\n", index, value)
+			case string:
+				fmt.Printf("list[%d] is a string and its value is %s\n", index, value)
+			case Person:
+				fmt.Printf("list[%d] is a Person and its value is %s\n", index, value)
+			default:
+				fmt.Printf("list[%d] is of a different type\n", index)
 		}
 	}
 }
